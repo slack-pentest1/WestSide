5d566bd4f86a9352e415cb94fe67309c2bdaab39
slack-pentest1 <slack-pentest1@Blueinfy.net>
2024-08-28 12:30:59 +0530

    Initial Commit

---
+++ WestSide/WestSideApp.swift
@@ -0,0 +1,17 @@
+//
+//  WestSideApp.swift
+//  WestSide
+//
+//  Created by Test Ios on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WestSideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
